17855f778e31f90f774068392a296c2daac4cc9f
suneelvanapalli <suneel.vanapalli@gmail.com>
2021-09-26 19:52:31 +1000

    Initial commit

---
+++ src/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+ngOnInit(): void {
+throw new Error('Method not implemented.');
+}
+
+  powers = ['Really Smart', 'Super Flexible',
+  'Super Hot', 'Weather Changer'];
+
+model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+submitted = false;
+
+onSubmit() { this.submitted = true; }
+
+newHero() {
+this.model = new Hero(42, '', '');
+}
+
+}
